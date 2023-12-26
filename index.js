c72ec57104aa5fed92ee593e2ef18ae4d7caa9c4
nelson parajuli <nelsonparajuli2064@gmail.com>
2023-12-26 12:42:50 +0545

    configure socket in server side and in our client side

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const { createServer } = require('node:http');
-const { join } = require('node:path');
+const { Server } = require('socket.io');
+const { join } = require('path'); // Add this line
 
 const app = express();
+
 const server = createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 server.listen(3000, () => {
